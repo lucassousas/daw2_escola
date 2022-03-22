cfbf21d5ed32a36d7746fa23578a42845262b800
Lucas Silva <lucas.ssilva1903@gmail.com>
2022-03-22 20:28:41 -0300

    templates

---
+++ storage/framework/views/5f1ee5d77f8a6f1558a3317445e6aaaf242fb47b.php
@@ -0,0 +1,3 @@
+<?php $__env->startSection("titulo", "Escola"); ?>
+
+<?php echo $__env->make("templates.main", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/lucas/Documentos/lucas/escola/resources/views/welcome.blade.php ENDPATH**/ ?>
